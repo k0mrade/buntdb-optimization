056619416d22da2afc9e2a7bcb6cd3ea0cbc4ffe
Dmitry Ivanko <di@onair.kh.ua>
2016-08-15 15:50:45 +0300

    Test AscendRange

--- bunt-optimization.go
+++ bunt-optimization.go
@@ -3,168 +3,171 @@ package main
 // URL
 // http://10.0.0.253:9002/join?nbiIP=10.0.0.241&wlan=58&reason=Un-Auth-Captive&loc=4f6666696365&mac=6c:aa:b3:1b:fa:50&uip=ENC1bf058f5336ccb5e32d7046a0c3f5ea9&url=http%3A%2F%2Fmazda.ua%2F&zoneName=ON+AIR+Office&client_mac=ENCa09ad3bc1b0383ef61e611b452b69a78d4efdec5e514fdde&sip=194.242.99.241&proxy=0&ssid=9002&wlanName=9002&dn=
 // KEY
-// AD000008 f0:3e:90:2e:4f:d0 ENCf9a8611a1c7139d045d8e5e451203ce2adc5e47c3dbe2157 ENC5da4cec7b32712378088a788f46e881d 1470113127851157834 1470113140954825778
+// 1470113127851157834 f0:3e:90:2e:4f:d0
 // VALUE
+// JSON SessionValue
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
-	"math/rand"
-	"strconv"
-	"strings"
 	"time"
 
-	"github.com/boltdb/bolt"
+	"github.com/jinzhu/now"
 	"github.com/tidwall/buntdb"
+	"github.com/tidwall/gjson"
 )
 
 func main() {
 	start := time.Now()
-	db, err := bolt.Open("_bolt.db", 0600, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-	fmt.Printf("Reading Bolt database %v\n", time.Since(start))
-	start = time.Now()
-	dbbunt, err := buntdb.Open("_bunt1M.db")
+	dbbunt, err := buntdb.Open("buntnew.db")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer dbbunt.Close()
 	fmt.Printf("Reading Bunt database --> %v\n", time.Since(start))
+
 	start = time.Now()
-	dbbunt.CreateIndex("AD000007", "AD000007 *", buntdb.IndexString)
-	fmt.Printf("String index AD000007 creation time --> %v\n", time.Since(start))
-	start = time.Now()
-	dbbunt.CreateIndex("AD000008", "AD000008 *", buntdb.IndexString)
-	fmt.Printf("String index AD000008 creation time --> %v\n", time.Since(start))
-	// dbbunt.CreateIndex("ENCf9a8611a1c7139d045d8e5e451203ce2adc5e47c3dbe2157", "* * ENCf9a8611a1c7139d045d8e5e451203ce2adc5e47c3dbe2157 * * *", buntdb.IndexString)
-	// dbbunt.CreateIndex("ENC225a6673d276676d6a9456482504256da4401487d286bf4f", "* * ENC225a6673d276676d6a9456482504256da4401487d286bf4f * * *", buntdb.IndexString)
-	// dbbunt.CreateIndex("AdActionTime", "*", buntdb.IndexJSON("AdActionTime"))
-	// fmt.Printf("JSON index creation time --> %v\n", time.Since(start))
+	dbbunt.CreateIndex("AdActionTime", "*", buntdb.IndexJSON("AdActionTime"))
+	fmt.Printf("JSON index AdActionTime creation time --> %v\n", time.Since(start))
 	idx, _ := dbbunt.Indexes()
 	fmt.Printf("Indexes list --> %v\n", idx)
-	// start := time.Now()
-	// // dbbunt.CreateIndex("AdActionTime", "*", buntdb.IndexJSON("AdActionTime"))
-	// // fmt.Printf("Время создания индекса AdActionTime --> %v\n", time.Since(start))
-	// // start = time.Now()
-	// dbbunt.CreateIndex("SZUrl.ClientMac", "*", buntdb.IndexJSON("SZUrl.ClientMac"))
-	// fmt.Printf("Время создания индекса SZUrl.ClientMac --> %v\n", time.Since(start))
-	// start = time.Now()
-	// dbbunt.CreateIndex("ApMac", "*", buntdb.IndexJSON("MAC"))
-	// fmt.Printf("Время создания индекса ApMac --> %v\n", time.Since(start))
 
-	// // Migrate from BoltDB to BuntDB. Generate addition keys
-	// bsl, _ := ByteSessionList(db)
-	// err = generateDB(bsl, 50, dbbunt)
+	var sk []string
+	un := make(map[string]string)
+	start = time.Now()
+	err = dbbunt.View(func(tx *buntdb.Tx) error {
+		tx.Ascend("AdActionTime", func(key, val string) bool {
+			mac := gjson.Get(val, "SZUrl.ClientMac")
+			// t := gjson.Get(val, "AdActionTime")
+			un[mac.String()] = val
+			sk = append(sk, val)
+			// fmt.Printf("Staion %v connects at %v\n", mac.String(), t.String()) //len(sk))
+			return true
+		})
+		return nil
+	})
+	fmt.Printf("Reading %v rows from BuntDB was --> %v\nUnique --> %v\n", len(sk), time.Since(start), len(un))
+
+	var sk1 []string
+	start = time.Now()
+	err = dbbunt.View(func(tx *buntdb.Tx) error {
+		tx.AscendRange("AdActionTime", "1470936865210540500", "1470999986453908700", func(key, val string) bool {
+			fmt.Println(key)
+			sk1 = append(sk1, key)
+			return true
+		})
+		fmt.Println(tx)
+		return nil
+	})
+	fmt.Printf("Range reading %v rows from BuntDB was --> %v\n", len(sk1), time.Since(start))
+
+	// start = time.Now()
+	// d := time.Unix(0, 1470790800000000000)
+	// sc, usc, err := DayCounter(d, dbbunt)
 	// if err != nil {
 	// 	fmt.Println(err)
 	// }
-
-	// t := time.Date(2016, time.January, 01, 0, 0, 0, 0, time.UTC)
-	// bm := now.New(t).BeginningOfMonth().UnixNano()
-	// ed := now.New(t).EndOfDay().UnixNano()
-	// em := now.New(t).EndOfMonth().UnixNano()
-	// eq := now.New(t).EndOfQuarter().UnixNano()
-	// fmt.Println(bm, ed, em)
-	kv3 := make(map[string]string)
-	start = time.Now()
-	err = dbbunt.View(func(tx *buntdb.Tx) error {
-		tx.Ascend("", func(key, val string) bool {
-			// tx.AscendRange("AdActionTime", string(bm), string(ed), func(key, val string) bool {
-			kv3[key] = val
-			// sv, _ := DecodeSessionValue([]byte(val))
-			// fmt.Printf("%s\n", val)
-			return true
-		})
-		return nil
-	})
-	fmt.Printf("Reading %v rows from BuntDB was --> %v\n", len(kv3), time.Since(start))
-	AD000007 := make(map[string]string)
-	start = time.Now()
-	err = dbbunt.View(func(tx *buntdb.Tx) error {
-		tx.Ascend("AD000007", func(key, val string) bool {
-			AD000007[key] = val
-			return true
-		})
-		return nil
-	})
-	fmt.Printf("Reading %v rows from index %v was --> %v\n", len(AD000007), "AD000007", time.Since(start))
-	AD000008 := make(map[string]string)
-	start = time.Now()
-	err = dbbunt.View(func(tx *buntdb.Tx) error {
-		tx.Ascend("AD000008", func(key, val string) bool {
-			AD000008[key] = val
-			return true
-		})
-		return nil
-	})
-	fmt.Printf("Reading %v rows from index %v was --> %v\n", len(AD000008), "AD000008", time.Since(start))
+	// fmt.Printf("Stations --> %v\nUnique stations --> %v\n", sc, usc)
+	// fmt.Printf("DayCounter retured value for --> %v\n", time.Since(start))
 }
 
-func generateDB(bss ByteSessions, multiplier int, db *buntdb.DB) error {
-	var err error
-	var randTime int64
-	var Avt float64
-	start := time.Now()
-	for _, bs := range bss {
-		start := time.Now()
-		err = db.Update(func(tx *buntdb.Tx) error {
-			for i := 0; i <= multiplier; i++ {
-				randTime = random(1451606400000000000+10000000000000*int64(i), 1483228799999999999)
-				// fmt.Println(time.Unix(0, randTime).Format("02-01-2006 15:04:05.000000000"))
-				sk := DecodeSessionKey(bs.Key)
-				sk.AdActionTime = randTime
-				if i == 2 || i == 3 {
-					sk.AdID = "AD000008"
-				}
-				sks := SessionKeyToString(sk)
-				sv, _ := DecodeSessionValue(bs.Value)
-				sv.AdActionTime = randTime
-				svn, _ := EncodeSessionValue(sv)
-				if sv.SZUrl.ClientMac != "" {
-					_, _, err := tx.Set(sks, string(svn), nil)
-					if err != nil {
-						return err
-					}
-				}
+func counter(hr [][]int64, idx string, db *buntdb.DB) (<-chan int, <-chan int, <-chan int) {
+	var lsk []string
+	un := make(map[string]string)
+	c1 := make(chan int)
+	c2 := make(chan int)
+	c3 := make(chan int)
+	for i, h := range hr {
+		go func(h []int64, i int) {
+			// fmt.Println(time.Unix(0, h[0]), time.Unix(0, h[1]))
+			start := time.Now()
+			err := db.View(func(tx *buntdb.Tx) error {
+				tx.AscendRange(idx, string(h[0]), string(h[1]), func(key, val string) bool {
+					fmt.Println(key)
+					return true
+					// date := strings.Split(key, " ")[0]
+					// if date > string(h[0]) && date < string(h[1]) {
+					// 	lsk = append(lsk, key)
+					// 	un[key] = val
+					// 	return true
+					// }
+					// // un[key[27:78]] = val
+					// // 	unAP1[key[9:26]] = val // AP MAC
+					// // lsk = append(lsk, key[135:])
+					// return false
+				})
+				return nil
+			})
+			if err != nil {
+				fmt.Println("Error")
 			}
-			return err
-		})
-		if err != nil {
-			return err
-		}
-		avt := time.Since(start).Seconds() / float64(multiplier)
-		Avt = Avt + avt
-		// fmt.Printf("Iteration %v\nSET per sec %v\nKey %v\n", i, avt, string(bs.Key))
+			fmt.Printf("During %v:00–%v:00\t was %v actions, %v unique mac from index %v was --> %v\n", i, (i + 1), len(lsk), len(un), idx, time.Since(start))
+			c1 <- i
+			c2 <- len(un)
+			c3 <- len(lsk)
+		}(h, i)
 	}
-	fmt.Printf("Migration to BuntDB was --> %v\nSET average time was --> %v\n", time.Since(start), Avt/25591)
-	return nil
+	return c1, c2, c3
 }
 
-func random(min, max int64) int64 {
-	rand.Seed(time.Now().Unix())
-	return rand.Int63n(max-min) + min
+// DayCounter ...
+func DayCounter(date time.Time, db *buntdb.DB) ([24]int, [24]int, error) {
+	var err error
+	var sk, usk [24]int
+	hr := HoursInDay(date)
+	c1, c2, c3 := counter(hr, "AdActionTime", db)
+	for i := 0; i < 24; i++ {
+		hour := <-c1
+		sk[hour] = <-c2
+		usk[hour] = <-c3
+	}
+	return sk, usk, err
 }
 
-// SessionKeyToString ecodes SessionKey to a string session key
-func SessionKeyToString(sk SessionKey) string {
-	st := strconv.FormatInt(sk.AdShowTime, 10)
-	at := strconv.FormatInt(sk.AdActionTime, 10)
-	enc := sk.AdID + " " + sk.ApMAC + " " + sk.StationMAC + " " + sk.StationIP + " " + st + " " + at
-	return enc
+// HoursInDay returns the array of two value – beginnig and end of hour.
+func HoursInDay(t time.Time) [][]int64 {
+	var hsbe [][]int64
+	hour := int64(3600000000000)
+	bd := now.New(t).BeginningOfDay()
+	bdn := bd.UnixNano()
+	edn := bdn + hour - 1 //now.New(bd).EndOfHour().UnixNano()
+	for i := 0; i < 24; i++ {
+		hsbe = append(hsbe, []int64{bdn, edn})
+		bdn = bdn + hour
+		edn = edn + hour
+	}
+	return hsbe
 }
 
-// SessionKey ...
-type SessionKey struct {
-	AdID         string
-	ApMAC        string
-	StationMAC   string
-	StationIP    string
-	AdShowTime   int64
-	AdActionTime int64
+// DaysInMonth returns the array of two value – beginnig and end of day.
+func DaysInMonth(t time.Time) [][]int64 {
+	var dsbe [][]int64
+	day := int64(86400000000000)
+	bm := now.New(t).BeginningOfMonth()
+	em := now.New(bm).EndOfMonth()
+	bdn := bm.UnixNano()
+	edn := bdn + day - int64(1)
+	for i := 1; i <= em.Day(); i++ {
+		dsbe = append(dsbe, []int64{bdn, edn})
+		bdn = bdn + day
+		edn = edn + day
+	}
+	return dsbe
+}
+
+// MonthInYear returns the array of two value – beginnig and end of month.
+func MonthInYear(t time.Time, loc *time.Location) [][]int64 {
+	var msbe [][]int64
+	year := now.New(t).Year()
+	for i := 1; i < 13; i++ {
+		fdm := time.Date(year, time.Month(i), 01, 0, 0, 0, 0, loc)
+		bm := now.New(fdm).BeginningOfMonth()
+		em := now.New(bm).EndOfMonth()
+		bmn := bm.UnixNano()
+		emn := em.UnixNano()
+		msbe = append(msbe, []int64{bmn, emn})
+	}
+	return msbe
 }
 
 // SessionValue ...
@@ -173,17 +176,16 @@ type SessionValue struct {
 	AdActionTime int64
 	AdStopTime   int64
 	UserAgent    UserAgent
-	SZUrl        SZURL
+	SZURL        SZURL
+	AD           SessionMarker
 }
 
-// ByteSession ...
-type ByteSession struct {
-	Key   []byte
-	Value []byte
+// SessionMarker ...
+type SessionMarker struct {
+	Name   string `json:"Name"`
+	Type   int    `json:"Type"`
+	Choice int    `json:"Choice,omitempty"`
 }
-
-// ByteSessions ...
-type ByteSessions []ByteSession
 
 // SZURL generated by SZ
 type SZURL struct {
@@ -224,71 +226,4 @@ type Engine struct {
 type Browser struct {
 	Name    string
 	Version string
-}
-
-// EncodeSessionKey ecodes SessionKey to the bytes session key
-// Bolt implementation
-func EncodeSessionKey(sk SessionKey) []byte {
-	st := strconv.FormatInt(sk.AdShowTime, 10)
-	at := strconv.FormatInt(sk.AdActionTime, 10)
-	enc := []byte(sk.AdID + "&" + sk.ApMAC + "&" + sk.StationMAC + "&" + sk.StationIP +
-		"&" + st + "&" + at)
-	return enc
-}
-
-// EncodeSessionValue ecodes SessionValue to the bytes session value
-// Bolt implementation
-func EncodeSessionValue(sv SessionValue) ([]byte, error) {
-	enc, err := json.Marshal(sv)
-	if err != nil {
-		return nil, err
-	}
-	return enc, nil
-}
-
-// DecodeSessionKey decodes the bytes session key to SessionKey
-// Bolt implementation
-func DecodeSessionKey(sessionkey []byte) SessionKey {
-	var key SessionKey
-	item := strings.Split(string(sessionkey), "&")
-	key.AdID = item[0]
-	key.ApMAC = item[1]
-	key.StationMAC = item[2]
-	key.StationIP = item[3]
-	key.AdShowTime, _ = strconv.ParseInt(item[4], 10, 64)
-	key.AdActionTime, _ = strconv.ParseInt(item[5], 10, 64)
-	return key
-}
-
-// DecodeSessionValue decodes the bytes session key to SessionValue
-// Bolt implementation
-func DecodeSessionValue(sessionvalue []byte) (SessionValue, error) {
-	var value SessionValue
-	err := json.Unmarshal(sessionvalue, &value)
-	if err != nil {
-		return value, err
-	}
-	return value, nil
-}
-
-// ByteSessionList returns list of ByteSessions
-// Bolt implementation
-func ByteSessionList(db *bolt.DB) (ByteSessions, error) {
-	var err error
-	session := new(ByteSession)
-	var sessions ByteSessions
-	err = db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("Session"))
-		sb := b.Bucket([]byte("Action")).Cursor()
-		for k, v := sb.First(); k != nil; k, v = sb.Next() {
-			session.Key = k
-			session.Value = v
-			sessions = append(sessions, *session)
-		}
-		return nil
-	})
-	if err != nil {
-		return nil, fmt.Errorf("Could not get the list of sessions %s", err)
-	}
-	return sessions, err
 }
